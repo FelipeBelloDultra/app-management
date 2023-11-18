74aafa7e0908bdbff2c816ec0df922e658e0955c
Felipe Bello Dultra <felipe_bello_dultra@hotmail.com>
2023-11-18 09:43:38 -0300

    feat: :rocket: create home page

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,3 +1,19 @@
+import Image from "next/image";
+
 export default function Home() {
-  return <h1>Hello</h1>;
+  return (
+    <div className="rounded-md bg-gray-50 border border-gray-100 flex items-center flex-col justify-center p-16 shadow-sm">
+      <div className="self-start mb-7">
+        <h1 className="font-bold text-2xl text-gray-700">
+          Welcome to <span className="text-cyan-500">App Management</span>!
+        </h1>
+
+        <p className="mt-3 text-gray-600">
+          Select a board and start <br /> organizing your tasks!
+        </p>
+      </div>
+
+      <Image src="/no-data.svg" alt="No data image" width="347" height="332" />
+    </div>
+  );
 }
