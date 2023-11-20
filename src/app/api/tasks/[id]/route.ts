45f7dd197bf4b3851b07c2cbf03a65cb38cf06cf
Felipe Bello Dultra <felipe_bello_dultra@hotmail.com>
2023-11-20 16:56:30 -0300

    chore: change pages to another directory

--- src/app/api/tasks/[id]/route.ts
+++ src/app/api/tasks/[id]/route.ts
@@ -24,6 +24,38 @@ export async function PUT(
   });
 
   revalidateTag(`boards-${boardId}`);
+  revalidateTag(`task-${updatedTask.id}`);
 
-  return NextResponse.json({ task: updatedTask }, { status: 201 });
+  return NextResponse.json({ data: updatedTask }, { status: 201 });
+}
+
+export async function GET(
+  request: NextRequest,
+  context: {
+    params: {
+      id: string;
+    };
+  }
+) {
+  const { id } = context.params;
+  const task = await prisma.task.findUniqueOrThrow({
+    where: {
+      id,
+    },
+    select: {
+      board_id: true,
+      descriptions: true,
+      tags: true,
+      id: true,
+      name: true,
+      created_at: true,
+      expires_at: true,
+      status: true,
+      updated_at: true,
+    },
+  });
+
+  revalidateTag(`task-${id}`);
+
+  return NextResponse.json({ data: task }, { status: 200 });
 }
